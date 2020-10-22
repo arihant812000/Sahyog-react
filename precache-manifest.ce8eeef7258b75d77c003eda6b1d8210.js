c40ed2148e4b2fe5166a20af9abedc018b4b416e
arihant jain <arihantjain812000@gmail.com>
2020-10-22 18:59:01 +0530

    Updates

---
+++ precache-manifest.ce8eeef7258b75d77c003eda6b1d8210.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21e88408088b517f3404778fc90feca3",
+    "url": "/Sahyog-react/index.html"
+  },
+  {
+    "revision": "ed33b115a5250caaa33e",
+    "url": "/Sahyog-react/static/css/main.79c18a6d.chunk.css"
+  },
+  {
+    "revision": "2d9e20e78b2cb59daa91",
+    "url": "/Sahyog-react/static/js/2.136bb08f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Sahyog-react/static/js/2.136bb08f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed33b115a5250caaa33e",
+    "url": "/Sahyog-react/static/js/main.9e637faa.chunk.js"
+  },
+  {
+    "revision": "7986aa9d8cd844a1552c",
+    "url": "/Sahyog-react/static/js/runtime-main.bf942b59.js"
+  }
+]);
